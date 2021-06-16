f94671c4ded47098bf0d18255c17138d1d2ed7af
beron <bleink@outlook.es>
2021-06-16 12:17:15 +0200

    Create FamilyGenerator along with Animal renderFamily functions to render groups as paternal families

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 
 public abstract class Animal : MonoBehaviour, IAnimal, IFactory
 {
     public static HashSet<GameObject> wholePopulation = new HashSet<GameObject>();
     public abstract HashSet<GameObject> Population { get ; set ; }
-    public bool gender = false, moving = false, adult, alone, asleep = false, death = false;
+    public bool gender = false, moving = false, adult, alone, asleep = false, death = false, female = false, male = false;
     public float hungry, exhaustion, lp, sensibility;
     public Vector3 size, sizePotential;
     public NavMeshAgent nav;
     public Rigidbody rig;
     public Animator ani;
+    public HashSet<GameObject> parents = new HashSet<GameObject>();
 
     public abstract AnimationsName animationsName { get; }
 
@@ -25,9 +24,9 @@ public abstract class Animal : MonoBehaviour, IAnimal, IFactory
         GameObject[] creatures = new GameObject[quantity];
         for (int idx = 0; quantity > idx; idx++)
         {
-            GameObject creature = Instantiate(animal, new Vector3(UnityEngine.Random.Range(0, range), respawnHeight, UnityEngine.Random.Range(0, range)), animal.transform.rotation);
+            GameObject creature = Instantiate(animal, new Vector3(Random.Range(0, range), respawnHeight, Random.Range(0, range)), animal.transform.rotation);
             Vector3 scale = creature.transform.localScale;
-            creature.transform.localScale = new Vector3(scale.x - UnityEngine.Random.Range(0.1f, 0.4f), scale.y - UnityEngine.Random.Range(0.1f, 0.4f), scale.z - UnityEngine.Random.Range(0.1f, 0.4f));
+            creature.transform.localScale = new Vector3(scale.x - Random.Range(0.1f, 0.4f), scale.y - Random.Range(0.1f, 0.4f), scale.z - Random.Range(0.1f, 0.4f));
             creatures[idx] = creature;
             Animal creatureScript = creature.GetComponent<Animal>();
             creatureScript.Population.Add(creature);
@@ -35,6 +34,77 @@ public abstract class Animal : MonoBehaviour, IAnimal, IFactory
         }
         return creatures;
     }
+    public static GameObject[] StaticRenderFamily(GameObject animal, int quantity, float parentsRandomRate, int minParentsCount, string parentalCare, Vector3 position, float height)
+    {
+        GameObject[] creatures = new GameObject[quantity];
+        Vector3[] positions = new Vector3[quantity];
+        int counter = 0;
+        for (int idx = -(quantity/2); quantity/2 > idx; idx+=counter)
+        {
+            positions[counter] = new Vector3((position.x * idx) + quantity, height, (position.z * idx) + quantity);
+            counter++;
+        }
+        bool paternalGender = true;
+        paternalGender = parentalCare switch
+        {
+            "maternal" => false,
+            "paternalGender" => true,
+            "biparental" => !paternalGender,
+            _ => true,
+        };
+        for (int idx = 0; minParentsCount > idx; idx++)
+        {
+            if (parentalCare == "biparental") paternalGender = !paternalGender;
+            GameObject creature = Instantiate(animal, positions[0], animal.transform.rotation);
+            Vector3 scale = creature.transform.localScale;
+            creature.transform.localScale = new Vector3(scale.x - Random.Range(0.1f, 0.4f), scale.y - Random.Range(0.1f, 0.4f), scale.z - Random.Range(0.1f, 0.4f));
+            creatures[idx] = creature;
+            Animal creatureScript = creature.GetComponent<Animal>();
+            creatureScript.Population.Add(creature);
+            if (paternalGender) creatureScript.male = true; 
+            else creatureScript.female = true;
+            wholePopulation.Add(creature);
+        }
+        float maxParentsCount = parentsRandomRate * quantity;
+        for (int idx = minParentsCount; quantity > idx; idx++)
+        {
+            GameObject creature = Instantiate(animal, positions[idx], animal.transform.rotation);
+            Vector3 scale = creature.transform.localScale;
+            creatures[idx] = creature;
+            Animal creatureScript = creature.GetComponent<Animal>();
+            if (maxParentsCount > minParentsCount)
+            {
+                creature.transform.localScale = new Vector3(scale.x - Random.Range(0.1f, 0.4f), scale.y - Random.Range(0.1f, 0.4f), scale.z - Random.Range(0.1f, 0.4f));
+                if (parentalCare == "biparental") paternalGender = !paternalGender;
+                minParentsCount++;
+                creatureScript.adult = true;
+            }
+            else
+            {
+                creature.transform.localScale = new Vector3(scale.x - Random.Range(0.3f, 0.7f), scale.y - Random.Range(0.3f, 0.7f), scale.z - Random.Range(0.3f, 0.7f));
+                paternalGender = Random.Range(1, 3) == 1 ? false : true;
+                creatureScript.adult = false;
+            }
+            if (paternalGender) creatureScript.male = true;
+            else creatureScript.female = true;
+            creatureScript.Population.Add(creature);
+            wholePopulation.Add(creature);
+        }
+        return creatures;
+    }
+    public static GameObject[] StaticRenderPaternalFamily(GameObject animal, int quantity, int parentsRandomRate, int minParentsCount, Vector3 position, float height)
+    {
+        return StaticRenderFamily(animal, quantity, parentsRandomRate, minParentsCount, "paternal", position, height);
+    }
+    public static GameObject[] StaticRenderPaternalFamilyRate(GameObject animal, int quantity, int parentsRandomRate, Vector3 position, float height)
+    {
+        return StaticRenderPaternalFamily(animal, quantity, parentsRandomRate, 0, position, height);
+    }
+    public static GameObject[] StaticRenderPaternalFamilyCount(GameObject animal, int quantity, int parentsCount, Vector3 position, float height)
+    {
+        return StaticRenderPaternalFamily(animal, quantity, 0, parentsCount, position, height);
+    }
+
     public virtual GameObject[] GenerateSquareRange(GameObject animal, int quantity, float range, float respawnHeight)
     {
         return Animal.StaticGenerateSquareRange(animal, quantity, range, respawnHeight);
---
+++ Assets/Scripts/FamilyGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FamilyGenerator : MonoBehaviour
+{
+    [Serializable]
+    public struct Subject
+    {
+        public GameObject gameObjectMale;
+        public GameObject gameObjectFemale;
+        public int quantity;
+        public Vector3 position;
+        public int positionHeight;
+        public int parentsCount;
+    }
+    public Subject[] subjects;
+    public GameObject center;
+    public float area, respawnHeight = 3;
+    public static HashSet<GameObject> bears = new HashSet<GameObject>(), rabbits = new HashSet<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(center.transform.position);
+        foreach (Subject animal in subjects)
+        {
+            Fill(animal.gameObjectMale, animal.quantity, animal.parentsCount, animal.position, animal.positionHeight);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // Initialize game
+    
+    public void Fill(GameObject animal, int quantity, int parentsCount, Vector3 position, float respawnHeight = 5)
+    {
+        IFactory factory = animal.GetComponent<IFactory>();
+        if ( factory != null)
+        {
+            Animal.StaticRenderPaternalFamilyCount(animal, quantity, parentsCount, position, respawnHeight);
+        } else
+        {
+            Animal.StaticRenderPaternalFamilyCount(animal, quantity, parentsCount, position, respawnHeight);
+        }
+    }
+}
