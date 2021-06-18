5ef2aab6d12ed0e91f7115a9a8573b53d3fed5f2
beron <bleink@outlook.es>
2021-06-18 16:44:04 +0200

    Add Sex class with male, female and SwitchGender statics

---
+++ Assets/Scripts/Sex.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class Sex
+{
+    public const char male = 'm';
+    public const char female = 'f';
+    public static char SwitchSex (char sex)
+    {
+        char actualSex = sex == female ? male : female;
+        return actualSex;
+    }
+}
