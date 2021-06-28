248d5b683c703fccc55e996b15c8568134bbd207
beron <bleink@outlook.es>
2021-06-28 15:42:52 +0200

    Add test class to store testers (script and gameobject wich contains properties to make tests) and TimteTest class to change by the inspector the timeSpeed in Time class

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Test : MonoBehaviour
+{
+    [Serializable]
+    public class Tester
+    {
+        public MonoBehaviour script;
+        public GameObject gameObject;
+        public Tester (MonoBehaviour pScript, GameObject pGameObject)
+        {
+            script = pScript;
+            gameObject = pGameObject;
+        }
+    }
+    public static List<Tester> testers = new List<Tester>();
+    public List<Tester> testerInstances;
+    private void Start ()
+    {
+        Test.testers.Add(new Tester(this, gameObject));
+        testerInstances = Test.testers;
+    }
+}
---
+++ Assets/Scripts/Time/TimeTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class TimeTest : MonoBehaviour
+{
+    public byte timeSpeed = 30;
+    private void Start()
+    {
+        Test.testers.Add(new Test.Tester(this, gameObject));
+    }
+    private void Update()
+    {
+        TimeController.timeController.SetTimeSpeed(timeSpeed);
+    }
+}
